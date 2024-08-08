dcc3265057d92d5cd3fb39d5af06f08840325aba
cherryan613 <cherry613@ewhain.net>
2024-08-08 14:37:15 +0900

    feat: Comment 기능 추가

---
+++ src/main/java/com/example/backend/repository/CommentRepository.java
@@ -0,0 +1,15 @@
+package com.example.backend.repository;
+
+import com.example.backend.entity.Comment;
+import com.example.backend.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    @Query(value = "SELECT * FROM comments WHERE product_code = :product_code", nativeQuery = true)
+    List<Comment> CommentByProductCode(Long product_code);
+}
